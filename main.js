77dd234c241312cca537fcef7b7e7f4ee657fb16
John Patrick Furaque <102823750+furaquejohn11@users.noreply.github.com>
2023-03-28 07:50:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+  const main = document.getElementById('container');
+
+  main.style.display = "none";
+  alert('Wala ako maisip grabe');
+  alert('Pero sige eto nalang');
+  main.style.display = "block";
+
+}
